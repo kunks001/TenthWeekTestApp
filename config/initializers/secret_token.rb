406cf45c27983c4144f02d26b5bf0d7ff1154f66
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-25 10:55:20 +0100

    Initial commit and set up rspec and twilio testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TenthWeekTestApp::Application.config.secret_key_base = '562569aef1abd7578e0013644327befb8adaad11f82f1682d7de664ad7a97f94da3620de70ff5ff90330f7499e56c3f8ebab8693da3a252d3736cf91546e5b47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TenthWeekTestApp::Application.config.session_store :cookie_store, key: '_TenthWeekTestApp_session'
